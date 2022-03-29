fbbb8c2c94bf3de66e2cd7d7c30422cf648a2a4a
Joachim-B <93817125+Joachim-B@users.noreply.github.com>
2022-03-29 15:40:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,14 @@
     
 </head>
 <body> 
-<header>
-    poiuytrfdghjklm
-</header>
+    <header>
+        <div class="menu_logo">
+            <img src="img/oceseat-header.png" alt="">
+        </div>
+        <div class="menu_camember">
+            <img src="img/menu_burger.svg" alt="">
+        </div>
+    </header>
   
 
     <main>
