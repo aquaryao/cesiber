35779130c7b0031496fff5305ca244d798b31318
Joachim-B <93817125+Joachim-B@users.noreply.github.com>
2022-03-29 15:42:57 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,1 +1,8 @@
-
+<header>
+    <div class="menu_logo">
+        <img src="../img/oceseat-header.png" alt="">
+    </div>
+    <div class="menu_camember">
+        <img src="../img/menu_burger.svg" alt="">
+    </div>
+</header>
